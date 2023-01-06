0c2cbbe230b34c6c46e5a1bbd5b7012f543a960d
sumit sheoran <sumitsheoran521@gmail.com>
2023-01-07 03:10:23 +0530

    07-01-2023

---
+++ Video - 3/script.js
@@ -0,0 +1,30 @@
+// The Switch Statement
+
+const day = "Thursday";
+
+switch(day) {
+    case "Monday" : 
+    console.log("Go to School");
+    break;
+    case "Tuesday" : 
+    console.log("Go to Stadium");
+    break;
+    case "Wednesday" : 
+    console.log("Go to Hospital");
+    break;
+    case "Thursday" : 
+    console.log("Go to hostle");
+    break;
+    case "Friday" : 
+    console.log("Go to Delhi");
+    break;
+    case "Saturday" : 
+    console.log("Rest");
+    break;
+    case "Sunday" : 
+    console.log("Movie");
+    break;
+}
+
+
+
