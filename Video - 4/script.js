c5119f0abcb42563f3bcdf7b2f0691ecff526e50
sumit sheoran <sumitsheoran521@gmail.com>
2023-01-09 00:54:28 +0530

    #

--- Video - 4/script.js
+++ Video - 4/script.js
@@ -74,14 +74,92 @@
 
 // Functions calling ohter functions
 
-function cutFruitPieces(fruit){
-    return fruit * 4;
-}
+// function cutFruitPieces(fruit){
+//     return fruit * 4;
+// }
 
-function fruitProcessor(apples, oranges) {
-    const applePieces = cutFruitPieces(apples);
-    const orangePieces = cutFruitPieces(oranges);
-    const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`
-    return juice;
-}
-console.log(fruitProcessor(2, 3));
+// function fruitProcessor(apples, oranges) {
+//     const applePieces = cutFruitPieces(apples);
+//     const orangePieces = cutFruitPieces(oranges);
+//     const juice = `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`
+//     return juice;
+// }
+// console.log(fruitProcessor(2, 3));
+
+
+
+// Reviewing Functions
+// const calcAge = function(birthYear){
+//     return 2023 - birthYear
+// }
+
+// const yearsUntilRetirement = function(birthYear, firstName) {
+//     const age = calcAge(birthYear);
+//     const retirement = 65 - age;
+//     if(retirement > 0){
+//         console.log(`${firstName} will retire in ${retirement} years`);
+//         return retirement;
+//     }else{
+//         console.log(`${firstName} is already retiered`);
+//         return -1 ;
+//     }
+// }
+// console.log(yearsUntilRetirement(1996, "Sumit"))
+// console.log(yearsUntilRetirement(1958, "Amit"))
+
+
+// Coding Challenge #1
+
+// const calcAverage = (match1, match2, match3)=> (match1 + match2 + match3)/3;
+// const scoreDolhpines = calcAverage(44, 23, 71);
+// const scoreKoalas = calcAverage(65, 54, 49);
+// function checkWinner(avgDolhpines, avgKoalas){
+//     avgDolhpines = scoreDolhpines;
+//     avgKoalas = scoreKoalas;
+//     if(avgDolhpines > avgKoalas*2){
+//         console.log(`Dolphine win!!! 🐬 (${avgDolhpines} vs ${avgKoalas})`);
+//     } else if(avgKoalas > avgDolhpines*2){
+//         console.log(`Koalas win!!! 🐨 (${avgKoalas} vs ${avgDolhpines})`);
+//     }
+//      else {
+//         console.log(`No winner ☹️ (${avgDolhpines} vs ${avgKoalas})`);
+//     }
+// }
+// checkWinner();
+
+
+// Arrays
+// const names = ["Sumit", "Archana", "Arshu", "Armit", 1993];
+// console.log(names);
+
+// const years = new Array(1991, 1992, 1993, 1994, "Sumit");
+// console.log(years);
+
+// const sumit = ['Sumit', 'Sheoran', 2023-1996];
+// console.log(sumit);
+
+// Exercise
+// const calcAge = function(birthYear){
+//     return 2023-birthYear;
+// }
+
+// const year = [1996, 1997, 2000, 2005, 2018];
+
+// console.log(calcAge(year[3]));
+
+
+// Basic Array Operations(Methods)
+const friend = ['Michael', 'Steve', 'Peter'];
+console.log(friend);
+friend.push('Jay'); // Add element at the end of array
+console.log(friend);
+
+friend.unshift('John') // add element in the starting of array
+console.log(friend);
+
+const poped = friend.pop(); // delete element from last
+console.log(poped);
+console.log(friend);
+
+friend.shift();
+console.log(friend);
