fc064b89a9bf7043c22f99afdbb3263b8b124d51
sumit sheoran <sumitsheoran521@gmail.com>
2023-01-20 20:08:15 +0530

    20-01-2023

--- Video - 7/script.js
+++ Video - 7/script.js
@@ -1,5 +1,23 @@
 "use strict";
+const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
 
-const x = '23';
-const calcAge = birthYear => 2023 - birthYear;
-console.log(calcAge(1996));
+const calcTempAmplitude = function (temps) {
+  let max = temps[0];
+  let min = temps[0];
+  for (let i = 0; i < temps.length; i++) {
+    const currTemp = temps[i];
+    if(typeof currTemp !== 'number'){
+        continue;
+    }
+    if (currTemp > max) {
+      max = currTemp;
+    }
+    if (currTemp < min) {
+      min = currTemp;
+    }
+  }
+  console.log(max, min);
+  return max - min;
+};
+const amplitube = calcTempAmplitude(temperatures);
+console.log(amplitube);
